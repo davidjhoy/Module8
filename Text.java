113fb6cd36cd7b1f71df51ae06cf64699234a2c8
david <davidjhoyjr@gmail.com>
2023-03-05 09:25:15 -0500

    directions

--- Text.java
+++ Text.java
@@ -15,21 +15,56 @@ public class Text {
             System.out.println("Please enter a valid url and number");
             return;
         }
-        try {
+        try{
             URL givenUrl = new URL(args[0]);
-        } 
-        catch(MalformedURLException ex){
+            URLConnection connect = givenUrl.openConnection();
+           
+            
+            try(
+                BufferedReader br = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+                PrintWriter pw = new PrintWriter(System.out, true);
+            ){
+                String input;
+                Integer direction = Integer.parseInt(args[1]);
+
+                //Split logic based on direction
+                if (direction > 0){
+                    for (int i = direction; i > 0; i--){
+                        if((input = br.readLine()) != null){
+                            pw.println(input);
+                        }
+                        
+                    }
+                } else if (direction < 0){
+
+                }else{
+                    while ((input = br.readLine()) != null){
+                        pw.println(input);
+                    }
+                }
+                
+    
+    
+            }
+            catch(MalformedURLException ex){
+                System.out.println("Please enter a valid URL");
+                return;
+            }
+            catch(IOException ex){
+                System.out.println("IO Exception");
+                return;
+            }
+
+            
+        } catch(MalformedURLException ex){
             System.out.println("Please enter a valid URL");
+        }
+        catch(IOException ex){
+            System.out.println("IO Exception");
             return;
         }
+       
         
-        try(
-            BufferedReader br = new BufferedReader(new InputStreamReader(givenUrl.openStream()));
-        ){
-            String input;
-
-            //parse the input integer to determine next steps
-        }
         
     }
 
